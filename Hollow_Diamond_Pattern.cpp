78798e41bd7e665e5a75261599ebaf2379c26b08
Navneet Singh Negi <33743031+navneetsn18@users.noreply.github.com>
2018-09-14 11:30:40 +0530

    Create Hollow_Diamond_Pattern.cpp

---
+++ Hollow_Diamond_Pattern.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,a;
+    cin>>a;
+    if(a%2!=0)
+    {
+        for(i=0;i<=a/2-1;i++)
+        {
+            for(j=0;j<a;j++)
+            {
+                if(i+j<=a/2)
+                cout<<"*    ";
+                else if(j-a/2>=i)
+                cout<<"*    ";
+                else
+                cout<<"     ";
+            }
+            cout<<"\n";
+        }
+        for(i=a/2;i<a;i++)
+        {
+            for(j=0;j<a;j++)
+            {
+                if(j<=i-a/2)
+                cout<<"*    ";
+                else if(i+j>=a+a/2-1)
+                cout<<"*    ";
+                else
+                cout<<"     ";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
